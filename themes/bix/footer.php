b3252e84c68ffa96ffd409dfa8e319ce28517812
MacEverett <maceverett3@gmail.com>
2016-09-07 10:36:41 -0700

    footer.php base html

--- themes/bix/footer.php
+++ themes/bix/footer.php
@@ -1,22 +1,34 @@
+
 <?php
 /**
  * The template for displaying the footer.
  *
- * @package RED_Starter_Theme
+ * @package inhabitent_Theme
  */
-
 ?>
-
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
+       <h1> Lets Talk </h1>
+			 <h2> Bix Bickson </h2>
+			 <h2> Bix(at)Bickson2.com</h2>
+		 <form class="" action="index.html" method="post">
+			 <input type="text" name="email" placeholder="Your Email">
+			 <input type="textbox" name="talk to bix" placeholder="Talk To Bix">
+		 </form>
+				<div class="overall-block">
+          <div class="block">
+						<div class="contact-info">
+							<p>
+								<i class="fa fa-facebook-square"></i>
+								<i class="fa fa-twitter-square"></i>
+								<i class="fa fa-google-plus-square"></i>
+							</p>
+						</div>
+					</div><!-- .block -->
+				</footer><!-- #colophon -->
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
 
 	</body>
-</html>
