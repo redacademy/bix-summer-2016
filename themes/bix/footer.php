769c348b9434c08edcad23d1b376822f0ef81d5d
MacEverett <maceverett3@gmail.com>
2016-09-07 10:42:55 -0700

    add link to bix linkdin

--- themes/bix/footer.php
+++ themes/bix/footer.php
@@ -20,9 +20,9 @@
           <div class="block">
 						<div class="contact-info">
 							<p>
-								<i class="fa fa-facebook-square"></i>
-								<i class="fa fa-twitter-square"></i>
-								<i class="fa fa-google-plus-square"></i>
+								<a class="fa fa-facebook-square"></a>
+								<a class="fa fa-twitter-square" ></a>
+								<a class="fa fa-linkedin" href="https://www.linkedin.com/in/bix-bickson-810b5921?authType=NAME_SEARCH&authToken=HVTT&locale=en_US&srchid=4904333481473269961197&srchindex=1&srchtotal=3&trk=vsrp_people_res_name&trkInfo=VSRPsearchId%3A4904333481473269961197%2CVSRPtargetId%3A77507148%2CVSRPcmpt%3Aprimary%2CVSRPnm%3Atrue%2CauthType%3ANAME_SEARCH"></a>
 							</p>
 						</div>
 					</div><!-- .block -->
