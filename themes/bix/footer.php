6c413924fb9d4e2d967967b07ca701ade93f690a
MacEverett <maceverett3@gmail.com>
2016-09-07 15:37:40 -0700

    merge conflict

--- themes/bix/footer.php
+++ themes/bix/footer.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * The template for displaying the footer.
@@ -12,25 +11,25 @@
        <div class="footer-top">
         <h1>Let's Talk</h1>
 				<h2> BIX BICKSON </h2>
-				<h2> Bix(at)Bickson2.Com </h2>
+				<h2> Bix(at)Bickson2.COM </h2>
 			</div>
-
-
- 				<form class ="footer-form" action="#" method="post">
-
- 					<input type="text" name="email" placeholder="Your Email" />
+        <form action="#" method="post">
+          <input type="text" name="email" placeholder="Your Email"/>
+ 					<textarea name="message" placeholder="Talk To Bix"></textarea>
+ 				<form action="#" method="post">
+          <input type="text" name="email" placeholder="Your Email" />
  					<textarea name="message" placeholder="Drop A Line..."></textarea>
  					<button>Send</button>
         </form>
-      </div>
-			<p>
-				<i class="fa fa-facebook-square"></i>
-				<i class="fa fa-twitter-square"></i>
-				<i class="fa fa-google-plus-square"></i>
-			</p>
+       <div class="icons">
+				 <p>
+								<i class="fa fa-facebook-square"></i>
+								<i class="fa fa-twitter-square"></i>
+								<i class="fa fa-google-plus-square"></i>
+							 </p>
+						 </div>
 			</footer>
-
-			<?php wp_footer(); ?>
-
-		</body>
-	</html>
+		</div>
+		<?php wp_footer(); ?>
+	</body>
+</html>
