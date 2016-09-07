5c4e5187f204bc961391b6d09ea454c9873f8ea8
MacEverett <maceverett3@gmail.com>
2016-09-07 16:56:33 -0700

    merge conflict round 2

--- themes/bix/footer.php
+++ themes/bix/footer.php
@@ -7,28 +7,21 @@
  */
 ?>
 			</div><!-- #content -->
+		<div class="footer-container">
+			<footer>
+       <div class="footer-top">
+        <h1>Talk To Us</h1>
+				<h2> BIX BICKSON </h2>
+				<h2> Bix(at)Bickson2.COM </h2>
+			</div>
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-       <h1> Lets Talk </h1>
-			 <h2> Bix Bickson </h2>
-			 <h2> Bix(at)Bickson2.com</h2>
-		 <form class="" action="index.html" method="post">
-			 <input type="text" name="email" placeholder="Your Email">
-			 <input type="textbox" name="talk to bix" placeholder="Talk To Bix">
-		 </form>
-				<div class="overall-block">
-          <div class="block">
-						<div class="contact-info">
-							<p>
-								<a class="fa fa-facebook-square"></a>
-								<a class="fa fa-twitter-square" ></a>
-								<a class="fa fa-linkedin" href="https://www.linkedin.com/in/bix-bickson-810b5921?authType=NAME_SEARCH&authToken=HVTT&locale=en_US&srchid=4904333481473269961197&srchindex=1&srchtotal=3&trk=vsrp_people_res_name&trkInfo=VSRPsearchId%3A4904333481473269961197%2CVSRPtargetId%3A77507148%2CVSRPcmpt%3Aprimary%2CVSRPnm%3Atrue%2CauthType%3ANAME_SEARCH"></a>
-							</p>
-						</div>
-					</div><!-- .block -->
-				</footer><!-- #colophon -->
-		</div><!-- #page -->
 
-		<?php wp_footer(); ?>
+ 				<form action="#" method="post">
 
-	</body>
+ 					<input type="text" name="email" placeholder="Your Email" />
+ 					<textarea name="message" placeholder="Talk To Bix"></textarea>
+ 					<button>Send</button>
+
+ 				</form>
+			</footer>
+		</div>
