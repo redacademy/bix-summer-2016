c89d39377454ff3498d3c6d0dac700046ac7cca6
Mark Valdez <Mark Valdez>
2016-09-08 16:58:57 -0700

    add transparency on scroll for navbar

--- themes/bix/js/main.js
+++ themes/bix/js/main.js
@@ -3,8 +3,19 @@ $(document).ready(function(){
 
 	console.log('hello');
 
+	// nav bar fading
+	window.onscroll = function() {
+	    if (window.scrollY > 76) {
+	    	$(".desktop-nav-wrapper .main-navigation").addClass("transparent-background");
+	    	// $(".desktop-nav-wrapper ul").addClass("transparent-background");
+	    }
+	    else {
+	    	$(".desktop-nav-wrapper .main-navigation").removeClass("transparent-background");
+	    	// $(".desktop-nav-wrapper ul").removeClass("transparent-background");
+	    };
+	},false;
    
-  $('.menu-button').on('click', function(){
+  	$('.menu-button').on('click', function(){
 	    $('.mobile-nav-wrapper #primary-menu').slideToggle(750);
 	    $('.mobile-nav-wrapper .menu-main-menu-container').fadeToggle(750);
 	    $('.fa-bars').slideToggle(750);
