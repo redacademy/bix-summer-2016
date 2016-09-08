094e3921f90e8861011aa3612819a524544e9227
wongerchris <wongerchris@gmail.com>
2016-09-08 10:42:03 -0700

    front-page styling/about author/section6

--- themes/bix/front-page.php
+++ themes/bix/front-page.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 		
-		<section class="home-banner">
+		<section class="home-banner"><!--Section 1-->
 		<?php if ( have_posts() ) : ?>
 
 			<?php if ( is_home() && ! is_front_page() ) : ?>
@@ -27,7 +27,7 @@ get_header(); ?>
 			</div>
 		</section>
 		
-		<section class="quote-wrapper-front">
+		<section class="quote-wrapper-front"><!--Section 2-->
 			<div class="quote-img"></div>
 			<div class="quote-wrapper-text">
 				<p class="quotes-text"><span class="quotes">"</span> There is an ocean between saying &amp; doing.</p>
@@ -36,7 +36,7 @@ get_header(); ?>
 			</div>
 		</section>
 
-		<section class="quote-wrapper-front">
+		<section class="quote-wrapper-front"><!--Section 3-->
 			<div class="quote-img-2"></div>
 			<div class="quote-wrapper-text">
 				<p class="quotes-text"><span class="quotes">"</span> It is the creation of <span class="quotes-text-orange">Knowledge</span> that will afford and Organization continued success <span class="quotes">"</span></p>
@@ -44,11 +44,11 @@ get_header(); ?>
 			</div>
 		</section>
 
-		<section class="sample-story-front">
+		<section class="sample-story-front"><!--Section 4-->
 			<a class="btn btn-download" href="#">Read a Sample Story</a>
 		</section>
 
-		<section class="home-book">
+		<section class="home-book"><!--Section 5-->
 			<img class="book-logo" src="<?php echo(get_template_directory_uri());?>/images/bix-logo-black.png" alt="bix logo"/>
 			<div class="home-book-text">
 				<h2 class="book-title">The Bix Book</h2>
@@ -58,19 +58,30 @@ get_header(); ?>
 			</div>
 			<div class="book-summary">
 			<div>
-				<h4>The Bix Book</h4>
+				<h3 class="bix-book-title">The Bix Book</h3>
 				<p class="summary-one">
 					This is a book of stories that I have experienced through my 30 years of consulting with corporations, institutions, non-profit organizations and individuals.
 				</p>
 				<p>
 					Stories that I hope allow you access, an opening, a possibility that allow your courage, your persistence, your compassion, your intelligence and your resilience to soar to its greatest heights knowing you may have to plumb your deepest depths to find it.  You can do this.  These are not my stories.  These are our stories said through my life to share with you.
 				</p>
-				<a href="#">Discover</a>
+				<a class="btn-discover" href="#">Discover</a>
 			</div>
 			</div>
 		</section>
 
+		<section class="home-author"><!--Section 6-->
+			<img class="author-img" src="<?php echo(get_template_directory_uri());?>/images/bix-bixson.png" alt="bix bixson the author"/>
+			<div>
+			<h3 class="home-subheading">About the Author</h3>
+				
+			<p>
+				Educated at Trinity University, Bix has been a member of the faculty of the Graduate Management Program at Antioch University in Seattle and served as the Chairman of the Board of Washington Works, a non-profit organization designed to provide women on public assistance the training and support needed to gain sustainable employment.
+			</p>
+			<a class="btn-explore" href="#">Explore</a>
+			</div>
 
+		</section>
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
