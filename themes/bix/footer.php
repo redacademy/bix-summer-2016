64bfa1ba148033be5556ef1ccb9922deeda24d5e
MacEverett <maceverett3@gmail.com>
2016-09-08 09:25:07 -0700

    9/8 rebase merge conflict fix

--- themes/bix/footer.php
+++ themes/bix/footer.php
@@ -11,12 +11,15 @@
        <div class="footer-top">
         <h1>Let's Talk</h1>
 				<h2> BIX BICKSON </h2>
-				<h2> Bix(at)Bickson2.COM </h2>
+				<h2> Bix<span class="footer-orange">(at)</span>Bickson2.COM </h2>
 			</div>
+			 <div class ="form-position">
  				<form action="#" method="post">
           <input type="text" name="email" placeholder="Your Email" />
  					<textarea name="message" placeholder="Drop A Line..."></textarea>
+					<br>
  					<button>Send</button>
+				<div>
         </form>
        <div class="icons">
 				 <p>
