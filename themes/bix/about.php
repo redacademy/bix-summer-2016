760f019b2fc71258f038073cabf3d91e91cb9f48
Mark Valdez <Mark Valdez>
2016-09-09 10:23:07 -0700

    add about page content, add scss file for about

--- themes/bix/about.php
+++ themes/bix/about.php
@@ -13,11 +13,49 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+			<div class="author-section">
+				<div class="author-section-title">
+					<h2>About Bix Bickson</h2>
+				</div>
+				<div class="author-section-intro">
+					<img src="" alt="">
+					<p>
+						<span>“ This book happened because my co-author, Anika Martins, said to me, “Have you written your book yet?” and I said to her “No, I need someone to say my book to.” She said, “Let’s go.” We began our conversations in coffee shops, Thai restaurants, Skype and by phone. With her generosity, she listened and formed conversations into these stories. I can’t thank her enough. “</span>
+						<span>-- Bix Bickson</span>
+					</p>
+				</div>
+				<div class="author-section-content-wrapper">
+					<div>
+						<h3>Why I'm Here</h3>
+						<p>
+							<span>I have been preparing for this conversation for over 70 years.</span>
+							<span>My life has been a journey dedicated to understanding and attempting to discover a pathway to resolve the over-arching dilemmas individuals and organizations face in today’s world. We all know the old answers will no longer work. </span>
+							<span>But where do we look for new answers?</span>
+							<span>This has been my quest.</span>
+						</p>
+					</div>
+					<div>
+						<h3>The world has changed.</h3>
+						<p>
+							<span>The only resource not subject to diminishing returns is knowledge. It is the creation of knowledge that will provide an organization with continued success. Our ability to learn – learning to learn – is the most needed transformation in ourselves and in our organizations if we are going to successfully meet the challenges of the global marketplace.</span>
+						</p>
+					</div>
+					<div>
+						<h3>The Bio</h3>
+						<p>
+							<span>Bix Bickson has developed a unique design and body of knowledge that has proven to be a reliable pathway for individuals and organizations to perform in extraordinary ways. Rather than offering a theory that leaves people with an explanation or description of the pathway, he gives direct access to inventing and fulfilling an audacious future.</span>
+					
+							<span>Educated at Trinity University, Bix has been a member of the faculty of the Graduate Management Program at Antioch University in Seattle and served as the Chairman of the Board of Washington Works, a non-profit organization designed to provide women on public assistance the training and support needed to gain sustainable employment. He was also a Board Member of Global Visionaries in Seattle, Washington a non-profit organization dedicated to educate and empower youth.</span>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+							<span>Bix has worked with major corporations, institutions, non-profit organizations and individuals throughout the world since 1985. </span>
 
-			<?php endwhile; // End of the loop. ?>
+							<span>Currently Bix is channelling his energy into Bickson2, a company where he is continuing helping others achieve impactful transformations. There he is joined by another Bickson, his son Joe. Together they make a tandem that is creating change by helping organizations revitalize their DNA.</span>
+						</p>
+					</div>
+				</div>
+			</div>
+
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
