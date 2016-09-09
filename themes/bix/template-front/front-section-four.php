4230eb6bec8f7b53e39b20b4e30820a6c42572eb
wongerchris <wongerchris@gmail.com>
2016-09-09 14:59:24 -0700

    front-page responsive section four

--- themes/bix/template-front/front-section-four.php
+++ themes/bix/template-front/front-section-four.php
@@ -1,15 +1,20 @@
 <section class="home-book"><!--Section 5-->
-	<img class="book-logo" src="<?php echo(get_template_directory_uri());?>/images/bix-logo-black.png" alt="bix logo"/>
-	<div class="home-book-text">
-		<h2 class="book-title">The Bix Book</h2>
-		<h4 class="book-subheading">Transformational Stories</h4>
-		<h3>Bix Bixon</h3>
-		<p><span>with</span> Annika Martins</p>	
+	<div class="book-logo-wrapper">
+		<img class="book-logo" src="<?php echo(get_template_directory_uri());?>/images/bix-logo-black.png" alt="bix logo"/>
+		<div class="home-book-text">
+			<h2 class="book-title">The Bix Book</h2>
+			<h4 class="book-subheading">Transformational Stories</h4>
+			<h3>Bix Bixon</h3>
+			<p>
+				<span>with</span> 
+				Annika Martins
+			</p>	
+		</div>
 	</div>
 	<div class="book-summary">
 		<div>
 			<h3 class="bix-book-title">The Bix Book</h3>
-			<?php echo CFS()->get( 'bix_book_summary' ); ?>
+				<?php echo CFS()->get( 'bix_book_summary' ); ?>
 			<a class="btn-discover" href="#">Discover</a>
 		</div>
 	</div>
