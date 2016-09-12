f6be012a4360d25fd5c76246446cd3c2eb6d7c45
wongerchris <wongerchris@gmail.com>
2016-09-12 13:18:55 -0700

    enqueue js file path to build folder

--- themes/bix/functions.php
+++ themes/bix/functions.php
@@ -91,9 +91,9 @@ function bix_scripts() {
 
 	wp_enqueue_script( 'jquery' );
 
-	wp_enqueue_script( 'flickity-js', get_template_directory_uri() . '/js/flickity.pkgd.js', array('jquery'), false, true);
+	wp_enqueue_script( 'flickity-js', get_template_directory_uri() . '/build/js/flickity.pkgd.min.js', array('jquery'), false, true);
 
-	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/js/main.js', array('jquery'), false, true);
+	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/build/js/main.min.js', array('jquery'), false, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
