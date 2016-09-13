890c979d451b6451c0bc0aff6c9936594958560e
wongerchris <wongerchris@gmail.com>
2016-09-13 15:11:07 -0700

    stories page responsive styling

--- themes/bix/index.php
+++ themes/bix/index.php
@@ -18,28 +18,26 @@ get_header(); ?>
 			</div>		
 		</section>
 
+			<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+				<?php if ( is_home() && ! is_front_page() ) : ?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+				<?php endif; ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'template-parts/content','stories' ); ?>
+
+				<?php endwhile; ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
 			<?php endif; ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content','stories' ); ?>
-
-			<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
--- themes/bix/template-parts/content-stories.php
+++ themes/bix/template-parts/content-stories.php
@@ -7,23 +7,24 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" class="stories-article" <?php post_class(); ?>>
-	
-	<header class="entry-header stories-main">
+	<article id="post-<?php the_ID(); ?>" class="stories-article" <?php post_class(); ?>>
 		
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php 
-	    $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 1200,800 ), false, '' ); 
-		?>
-		<div class="featured-img" style="background-image:url(<?php echo $src[0]; ?>)">	
-			<img class="stories-ribbon" src="<?php echo(get_template_directory_uri());?>/images/yellow-ribbon.svg" alt="yellow ribbon"/>
-			<h3 class="story-number"><?php echo CFS()->get( 'story_number' ); ?></h3>
-		</div>
-	<?php endif; ?>
+		<header class="entry-header stories-main">
+			
+			<?php if ( has_post_thumbnail() ) : ?>
+				<?php 
+		    $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 1200,800 ), false, '' ); 
+			?>
+			<div class="featured-img" style="background-image:url(<?php echo $src[0]; ?>)">	
+				<img class="stories-ribbon" src="<?php echo(get_template_directory_uri());?>/images/yellow-ribbon.svg" alt="yellow ribbon"/>
+				<h3 class="story-number"><?php echo CFS()->get( 'story_number' ); ?></h3>
+			</div>
+		<?php endif; ?>
 
-		<div class="entry-content">
-			<?php the_title( sprintf( '<h2 class="stories-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-content -->
-	</header><!-- .entry-header -->	
-</article><!-- #post-## -->
+			<div class="entry-content">
+				<?php the_title( sprintf( '<h2 class="stories-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+				<?php the_excerpt(); ?>
+			</div><!-- .entry-content -->
+		</header><!-- .entry-header -->	
+	</article><!-- #post-## -->
+
