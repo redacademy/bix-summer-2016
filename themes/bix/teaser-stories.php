6d224a256eb8a41329087ee9dbb6859e7fbc7329
wongerchris <wongerchris@gmail.com>
2016-09-14 12:01:53 -0700

    add teaser page template file

---
+++ themes/bix/teaser-stories.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: Teaser-Stories Template Page
+*/
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<section class="about-banner">
+			<div class="about-header">
+				<img class="site-logo" src="<?php echo(get_template_directory_uri());?>/images/bix-logo.png" alt="bix logo"/>
+				<h2 class="about-title">About Bix</h2>
+				<p>About the Author</p>
+			</div>
+		</section>
+			
+	</div>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
