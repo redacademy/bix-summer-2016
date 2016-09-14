5d034b413cc616a54ab4365bc79d32b82b2a0378
MacEverett <maceverett3@gmail.com>
2016-09-13 17:00:12 -0700

    replace code for front-model.scss

--- themes/bix/bix-book.php
+++ themes/bix/bix-book.php
@@ -72,45 +72,7 @@ get_header(); ?>
 			</section>
 
 	<section class="book-section3 book-purchase">
-
-
-
-
-<!--flickity Carousel-->
-			<div class="main-carousel">
-  			<div class="carousel-cell bix-carousel-1">
-
-	  				<div class="bix-carousel-img bix-carousel-img-1"></div>
-	  				<div class="bix-carousel-info bix-carousel-info-1">
-							<ul>
-								<li>38 quality stories</li>
-								<li>Interactive workbook section for every chapter</li>
-								<li>Backed by 30 years of real world experience </li>
-								<li>Transformational content</li>
-								<li>Empower yourself to change</li>
-							</ul>
-						</div>
-
-  			</div>
-  			<div class="carousel-cell bix-carousel-2">
-  				<div class="bix-carousel-img bix-carousel-img-2"></div>
-  				<div class="bix-carousel-info bix-carousel-info-2">
-						<p>
-						30 years of extensive experience in the field has allowed me to produce the messages delivered in easy-to-read stories, each supplemented with workbook practices to enhance your learning
-					</p>
-					</div>
-  			</div>
-  			<div class="carousel-cell bix-carousel-3">
-  				<div class="bix-carousel-img bix-carousel-img-3"></div>
-  				<div class="bix-carousel-info bix-carousel-info-3">
-						<p>
-						Create impactful change with the powerful messages available in this book. Get the edge you need to empower your aspirations and make changing circumstances work for the ever changing organism that you are.
-					</p>
-					</div>
-  			</div>
-			</div>
-
-			<section class="book-purchase">
+    <section class="book-purchase">
 				<div class="book-title-wrapper">
 					<h2 class="the-bix-book-title"><span>Paying for</span> the Bix Book</h2>
 				</div>
