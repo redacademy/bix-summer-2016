f694b8720a9375df4e6e13f3d2681b8fce19bc43
MacEverett <maceverett3@gmail.com>
2016-09-14 13:28:20 -0700

    adding highlight modal

---
+++ themes/bix/template-parts/content-highlight-modal.php
@@ -0,0 +1,18 @@
+<!--Start of Modal -->
+<div class="modal">
+    <input class="modal__trigger" id="modal__trigger" type="checkbox" />
+    <p>Download <span class="blue-books"> The Bix Book </span> first </p>
+    <label for="modal__trigger" class="btn btn-donate">Donate Later</label>
+    <div class="modal__overlay" role="dialog" aria-labelledby="modal__title" aria-describedby="modal_desc">
+        <div class="modal__wrap">
+            <label class="close" for="modal__trigger">&#10006;</label>
+            <h2 id="modal__title">Want To Download The Bix Book?</h2>
+            <div class="highlight-example">
+              <img src="http://mathbits.com/MathBits/StudentResources/GraphPaper/14by14%20axes.jpg"></img>
+
+        <input class="email-unlock" id="modal__desc" type="text" name="email" placeholder="Your Email"/>
+            <button class="modal-button" href="#"> Unlock Download </button>
+        </div>
+    </div>
+</div>
+<!--End of Modal -->
