bca5fd17a56ad435653ea2785c9f19ced80c1bbe
Mark Valdez <Mark Valdez>
2016-09-14 11:31:29 -0700

    finish bottom toolbar for desktop

--- themes/bix/single.php
+++ themes/bix/single.php
@@ -30,16 +30,22 @@ get_header(); ?>
 			<div class="bottom-toolbar-wrapper">
 				<div class="bottom-toolbar">
 					<div class="tools">
-						
+						<i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+						<i class="fa fa-comments" aria-hidden="true"></i>
+						<i class="fa fa-share-square-o" aria-hidden="true"></i>
 					</div>
 					<div class="next-story">
-						<p>Read the Next Story</p>
-						<?php next_post_link('%link'); ?>
+						<?php if (next_post_link()) { ?>
+							<p>Read the Next Story</p>
+							<?php next_post_link('%link'); ?>
+						<?php } else { ?>
+							<p>Want more stories?</p>
+							<a href="#">What is offered in the bix book</a>
+						<?php }; ?>
 					</div>
 				</div>
 			</div>
 		</div>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
