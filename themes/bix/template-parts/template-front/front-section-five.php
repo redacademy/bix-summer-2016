b2a681b343d1eff38854398cb932804725e25095
MacEverett <maceverett3@gmail.com>
2016-09-15 13:48:57 -0700

    combine highlight and social media modals

---
+++ themes/bix/template-parts/template-front/front-section-five.php
@@ -0,0 +1,15 @@
+<section class="home-author"><!--Section 6-->
+	<div class="author-img-wrapper">
+		<img class="author-img" src="	<?php echo CFS()->get( 'author_picture' ); ?>" alt="bix bixson the author"/>
+	</div>
+	<div class="home-author-info">
+		<div>
+			<h3 class="home-subheading">About the Author</h3>
+			<?php echo CFS()->get( 'bix_bixson_author_summary' ); ?>
+			<a class="btn-explore" href="#">Explore</a>
+		</div>
+	</div>
+</section>
+
+
+
---
+++ themes/bix/template-parts/template-front/front-section-three.php
@@ -0,0 +1,3 @@
+<section class="sample-story-front"><!--Section 4-->
+	<a class="btn btn-download" href="#">Read a Sample Story</a>
+</section>
