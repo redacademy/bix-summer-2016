a6cde70206f001889721d890f41555d1ae37e30d
MacEverett <maceverett3@gmail.com>
2016-09-14 18:43:12 -0700

    add footer buttons to bar

--- themes/bix/template-parts/content-highlight-modal.php
+++ themes/bix/template-parts/content-highlight-modal.php
@@ -1,20 +1,20 @@
 
-<!--Start of Modal -->
-<div class="modal">
-    <input class="modal__trigger" id="modal__trigger" type="checkbox"></input>
-    <label for="modal__trigger" class="fa fa-pencil-square-o" aria-hidden="true"></label>
-    <div class="modal__overlay" role="dialog" aria-labelledby="modal__title" aria-describedby="modal_desc">
-        <div class="modal__wrap">
-            <label class="close" for="modal__trigger">&#10006;</label>
+<!--Start of highlight-modal -->
+<div class="highlight-modal">
+    <input class="highlight-modal__trigger" id="highlight-modal__trigger" type="checkbox"></input>
+    <label for="highlight-modal__trigger" class="fa fa-pencil-square-o" aria-hidden="true"></label>
+    <div class="highlight-modal__overlay" role="dialog" aria-labelledby="highlight-modal__title" aria-describedby="highlight-modal_desc">
+        <div class="highlight-modal__wrap">
+            <label class="close" for="highlight-modal__trigger">&#10006;</label>
               <i class="fa fa-pencil fa-5x"></i>
-            <h2 id="modal__title">Highlight</h2>
-            <h5 class="modal__wrap-h5">Is there an interesting quote from the story that speaks to you? </h5>
-          <h5 class="modal__wrap-h5">Use the highlight tool to share your thoughts </h5>
+            <h2 id="highlight-modal__title">Highlight</h2>
+            <h5 class="highlight-modal__wrap-h5">Is there an interesting quote from the story that speaks to you? </h5>
+          <h5 class="highlight-modal__wrap-h5">Use the highlight tool to share your thoughts </h5>
           <div class="highlight-example">
             <p> Example: </p>
          </div>
-            <button class="modal-button" href="#"> Unlock Download </button>
+            <button class="highlight-modal-button" href="#"> Unlock Download </button>
         </div>
     </div>
 </div>
-<!--End of Modal -->
+<!--End of highlight-modal -->
--- themes/bix/template-parts/content-social-media-modal.php
+++ themes/bix/template-parts/content-social-media-modal.php
@@ -1,20 +1,20 @@
-<!--Start of Modal -->
-<div class="modal">
-    <input class="modal__trigger" id="modal__trigger" type="checkbox" />
-    <label for="modal__trigger" class="fa fa-share-square-o" aria-hidden="true"></label>
-    <div class="modal__overlay" role="dialog" aria-labelledby="modal__title" aria-describedby="modal_desc">
-        <div class="modal__wrap">
-            <label class="close-out" for="modal__trigger">&#10006;</label>
+<!--Start of social-media-modal -->
+<div class="social-media-modal">
+    <input class="social-media-modal__trigger" id="social-media-modal__trigger" type="checkbox" />
+    <label for="social-media-modal__trigger" class="fa fa-share-square-o" aria-hidden="true"></label>
+    <div class="social-media-modal__overlay" role="dialog" aria-labelledby="social-media-modal__title" aria-describedby="social-media-modal_desc">
+        <div class="social-media-modal__wrap">
+            <label class="close-out" for="social-media-modal__trigger">&#10006;</label>
             <h1> Share This </h1>
-            <h5 class="modal__wrap-h5"> Share this on your favourite social media page</h5>
+            <h5 class="social-media-modal__wrap-h5"> Share this on your favourite social media page</h5>
         <ul class="icons-list">
 	           <li><a class="icon-size" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
 	           <li><a class="icon-size" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
 	           <li><a class="icon-size" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
        </ul>
-       <button class="modal-button" href="#"> Done </button>
+       <button class="social-media-modal-button" href="#"> Done </button>
    </div>
  </div>
 </div>
 
-<!--End of Modal -->
+<!--End of social-media-modal -->
