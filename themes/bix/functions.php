470b73efafd5bd9777b7eb19eb2099c829bdd43e
wongerchris <wongerchris@gmail.com>
2016-09-15 15:28:35 -0700

    story archive page template name

--- themes/bix/functions.php
+++ themes/bix/functions.php
@@ -80,53 +80,6 @@ function bix_minified_css( $stylesheet_uri, $stylesheet_dir_uri ) {
 add_filter( 'stylesheet_uri', 'bix_minified_css', 10, 2 );
 
 
-// /**
-//  * Excerpt Adjustments
-//  */
-// function bix_wp_trim_excerpt( $text ) {
-// 	$raw_excerpt = $text;
-	
-// 	if ( '' == $text ) {
-// 		// retrieve the post content
-// 		$text = get_the_content('');
-		
-// 		// delete all shortcode tags from the content
-// 		$text = strip_shortcodes( $text );
-		
-// 		$text = apply_filters( 'the_content', $text );
-// 		$text = str_replace( ']]>', ']]&gt;', $text );
-		
-// 		// indicate allowable tags
-// 		$allowed_tags = '<p>,<a>,<em>,<strong>,<blockquote>,<cite>';
-// 		$text = strip_tags( $text, $allowed_tags );
-		
-// 		// change to desired word count
-// 		$excerpt_word_count = 35;
-// 		$excerpt_length = apply_filters( 'excerpt_length', $excerpt_word_count );
-		
-// 		// create a custom "more" link
-// 		$excerpt_end = '<span>[...]</span><p><a href="' . get_permalink() . '" class="btn btn-stories">Explore</a></p>'; // modify excerpt ending
-// 		$excerpt_more = apply_filters( 'excerpt_more', ' ' . $excerpt_end );
-		
-// 		// add the elipsis and link to the end if the word count is longer than the excerpt
-// 		$words = preg_split( "/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY );
-		
-// 		if ( count( $words ) > $excerpt_length ) {
-// 			array_pop( $words );
-// 			$text = implode( ' ', $words );
-// 			$text = $text . $excerpt_more;
-// 		} else {
-// 			$text = implode( ' ', $words );
-// 		}
-// 	}
-	
-// 	return apply_filters( 'wp_trim_excerpt', $text, $raw_excerpt );
-// }
-
-// remove_filter( 'get_the_excerpt', 'wp_trim_excerpt' );
-// add_filter( 'get_the_excerpt', 'bix_wp_trim_excerpt' );
-
-
 /**
  * Enqueue scripts and styles.
  */
