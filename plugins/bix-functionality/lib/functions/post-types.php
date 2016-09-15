3183542dbba94e860943b3ef576680e8a0e17eca
wongerchris <wongerchris@gmail.com>
2016-09-15 11:06:01 -0700

    add custom post type

--- plugins/bix-functionality/lib/functions/post-types.php
+++ plugins/bix-functionality/lib/functions/post-types.php
@@ -6,3 +6,55 @@
  */
 
 // Add your custom post types here...
+
+function bix_register_stories_cpt() {
+
+	$labels = array(
+		'name'                  => 'Stories',
+		'singular_name'         => 'Story',
+		'menu_name'             => 'Stories',
+		'name_admin_bar'        => 'Story',
+		'archives'              => 'Story Archives',
+		'parent_item_colon'     => 'Parent Story:',
+		'all_items'             => 'All Stories',
+		'add_new_item'          => 'Add New Story',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Story',
+		'edit_item'             => 'Edit Story',
+		'update_item'           => 'Update Story',
+		'view_item'             => 'View Story',
+		'search_items'          => 'Search Story',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Story Image',
+		'set_featured_image'    => 'Set Story image',
+		'remove_featured_image' => 'Remove Story image',
+		'use_featured_image'    => 'Use as Story image',
+		'insert_into_item'      => 'Insert into Story',
+		'uploaded_to_this_item' => 'Uploaded to this Story',
+		'items_list'            => 'Stories list',
+		'items_list_navigation' => 'Stories list navigation',
+		'filter_items_list'     => 'Filter Stories list',
+	);
+	$args = array(
+		'label'                 => 'Story',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-welcome-write-blog',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => 'Products',
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'story', $args );
+
+}
+add_action( 'init', 'bix_register_stories_cpt', 0 );
