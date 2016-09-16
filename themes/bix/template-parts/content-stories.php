d5095ca78604c83a91a40a43adf75cdd4f0b7061
Mark Valdez <Mark Valdez>
2016-09-16 11:04:44 -0700

    add checks for empty content in stories page

--- themes/bix/template-parts/content-stories.php
+++ themes/bix/template-parts/content-stories.php
@@ -17,13 +17,24 @@
 				?>
 				<div class="featured-img" style="background-image:url(<?php echo $src[0]; ?>)">	
 					<img class="stories-ribbon" src="<?php echo(get_template_directory_uri());?>/images/yellow-ribbon.svg" alt="yellow ribbon"/>
-					<h3 class="story-number"><a href="<?php the_permalink(); ?> "><span>Story</span> <?php echo CFS()->get( 'story_number' ); ?></a></h3>
+					<h3 class="story-number"><a href="<?php the_permalink(); ?> "><span>Story</span> 
+						<?php if ( CFS()->get( 'story_number' ) ) : ?>
+							<?php echo CFS()->get( 'story_number' ); ?>
+						<?php endif; ?>
+					</a></h3>
 				</div>
 			<?php endif; ?>
 
 			<div class="entry-content">
-				<?php the_title( sprintf( '<h2 class="stories-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-				<?php echo CFS()->get( 'story_excerpt');?>
+
+				<?php if ( the_title() ) : ?>
+					<?php the_title( sprintf( '<h2 class="stories-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+				<?php endif; ?>
+
+				<?php if ( CFS()->get( 'story_excerpt' ) ) : ?>
+					<?php echo CFS()->get( 'story_excerpt');?>
+				<?php endif; ?>
+				
 				<a href="<?php the_permalink(); ?> " class="btn btn-stories">Read More</a>
 
 				
