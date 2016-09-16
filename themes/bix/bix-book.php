65810bae69122779bf47cff906355170373369b2
Mark Valdez <Mark Valdez>
2016-09-16 11:17:03 -0700

    add checks for empty content in bix book page

--- themes/bix/bix-book.php
+++ themes/bix/bix-book.php
@@ -25,58 +25,67 @@ get_header(); ?>
 			<section class="book-section1">
 			<img class="ribbon" src="<?php echo(get_template_directory_uri());?>/images/yellow-ribbon.svg" alt="yellow ribbon"/>
 			<h2 class="ribbon-heading">The Bix Book</h2>
+				
+				<?php if ( CFS()->get( 'bix_book_description' ) ) : ?>
+					<div class="book-section1-inside">
+						<?php echo CFS()->get( 'bix_book_description' ); ?>
+					</div>
+				<?php endif; ?>
 
-				<div class="book-section1-inside">
-
-				<?php echo CFS()->get( 'bix_book_description' ); ?>
-
-
-
-				</div>
 			</section>
 			<div class="book-title-wrapper book-title-wrapper-2">
 				<h2 class="the-bix-book-title">Whats in the bix book</h2>
 			</div>
 
-
-			<section class="book-section2">
-				<img class="ribbon" src="<?php echo(get_template_directory_uri());?>/images/yellow-ribbon.svg" alt="yellow ribbon"/>
-				<h2 class="ribbon-heading">Features</h2>
-				<div class="main-carousel">
-				<!--flickity Carousel-->
-	  			<div class="carousel-cell bix-carousel-1">
-	  				<div class="bix-carousel-img bix-carousel-img-1"></div>
-	  				<div class="bix-carousel-info bix-carousel-info-1">
-	  					<?php echo CFS()->get( 'feature_description_1' ); ?>
-						</div>
-	  			</div>
-	  			<div class="carousel-cell bix-carousel-2">
-	  				<div class="bix-carousel-img bix-carousel-img-2"></div>
-	  				<div class="bix-carousel-info bix-carousel-info-2">
-							<?php echo CFS()->get( 'feature_description_2' ); ?>
-						</div>
-
-	  			</div>
-	  			<div class="carousel-cell bix-carousel-3">
-	  				<div class="bix-carousel-img bix-carousel-img-3"></div>
-	  				<div class="bix-carousel-info bix-carousel-info-3">
-	  					<?php echo CFS()->get( 'feature_description_3' ); ?>
-						</div>
-	  			</div>
-				</div>
-			</section>
+			<?php if ( CFS()->get( 'feature_description_1' ) || CFS()->get( 'feature_description_2' ) || CFS()->get( 'feature_description_3' ) ) : ?>
+				<section class="book-section2">
+					<img class="ribbon" src="<?php echo(get_template_directory_uri());?>/images/yellow-ribbon.svg" alt="yellow ribbon"/>
+					<h2 class="ribbon-heading">Features</h2>
+					<div class="main-carousel">
+					<!--flickity Carousel-->
+					<?php if ( CFS()->get( 'feature_description_1' ) ) : ?>
+			  			<div class="carousel-cell bix-carousel-1">
+			  				<div class="bix-carousel-img bix-carousel-img-1"></div>
+			  				<div class="bix-carousel-info bix-carousel-info-1">
+			  					<?php echo CFS()->get( 'feature_description_1' ); ?>
+							</div>
+			  			</div>
+		  			<?php endif; ?>
+		  			<?php if ( CFS()->get( 'feature_description_2' ) ) : ?>
+			  			<div class="carousel-cell bix-carousel-2">
+			  				<div class="bix-carousel-img bix-carousel-img-2"></div>
+			  				<div class="bix-carousel-info bix-carousel-info-2">
+								<?php echo CFS()->get( 'feature_description_2' ); ?>
+							</div>
+			  			</div>
+		  			<?php endif; ?>
+		  			<?php if ( CFS()->get( 'feature_description_3' ) ) : ?>
+			  			<div class="carousel-cell bix-carousel-3">
+			  				<div class="bix-carousel-img bix-carousel-img-3"></div>
+			  				<div class="bix-carousel-info bix-carousel-info-3">
+			  					<?php echo CFS()->get( 'feature_description_3' ); ?>
+							</div>
+			  			</div>
+		  			<?php endif; ?>
+					</div>
+				</section>
+			<?php endif; ?>
 
 			<section id="buy-book-anchor" class="book-section3 book-purchase">
 				<div class="book-title-wrapper">
 					<h2 class="the-bix-book-title"><span>Paying for</span> the Bix Book</h2>
 				</div>
 				<div class="book-purchase-info">
-					<div>
-						<?php echo CFS()->get( 'paying_for_the_book_description' ); ?>
-					</div>
-					<p class="book-buy">
-						<?php echo CFS()->get( 'call_to_action' ); ?>
-					</p>
+					<?php if ( CFS()->get( 'paying_for_the_book_description' ) ) : ?>
+						<div>
+							<?php echo CFS()->get( 'paying_for_the_book_description' ); ?>
+						</div>
+					<?php endif; ?>
+					<?php if ( CFS()->get( 'call_to_action' ) ) : ?>
+						<p class="book-buy">
+							<?php echo CFS()->get( 'call_to_action' ); ?>
+						</p>
+					<?php endif; ?>
 					<p class="book-buy-light">Customize your contribution amount in  <i class="fa fa-cc-paypal"></i>
 					</p>
 
