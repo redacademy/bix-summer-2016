3673d238ada40cea04e1c030a7445667005277c3
MacEverett <maceverett3@gmail.com>
2016-09-20 08:01:56 -0700

    style thankyou and dd modal

--- themes/bix/template-parts/content-modal.php
+++ themes/bix/template-parts/content-modal.php
@@ -5,5 +5,6 @@
             <h2 id="dd-modal-title">Want To Download The Bix Book?</h2>
             <h5 class="dd-modal-h5"> Submit Email to Start Downloading the BIX BOOK </h5>
             <input class="email-unlock" id="modal__desc" type="text" name="email" placeholder="Your Email"/>
+            <a class="dd-modal-button" href="#"> Submit Email </a>
   </div>
 </div>
--- themes/bix/template-parts/content-thank-you.php
+++ themes/bix/template-parts/content-thank-you.php
@@ -1,17 +1,10 @@
 <!--Start of thank-you-modal -->
 <div class="thank-you-modal">
-    <input class="thank-you-modal__trigger" id="thank-you-modal__trigger" type="checkbox" />
-    <label for="thank-you-modal__trigger" id="modal-button" aria-hidden="true">Download</label>
-    <div class="thank-you-modal__overlay" role="dialog" aria-labelledby="thank-you-modal__title" aria-describedby="thank-you-modal_desc">
-        <div class="thank-you-modal__wrap">
-         			<div>
-              <h1 class="thank-you-title"> Thanks for <span class="footer-orange">Downloading<span></h1>
+  <div class="thank-you-modal-wrapper"
+              <h2 class="thank-you-title"> Thanks for <span class="footer-orange">Downloading<span></h1>
          			<img src="<?php echo(get_template_directory_uri());?>/images/thank-you-image.png" alt="thank you"/>
-							<h2> Listening is having the person speaking experience being heard <br>- "Bix Bickson"</h2>
-							<h5> come back and join the conversation </h5>
-              <button for="thank-you-modal__trigger" id="thank-you-modal-button"> Done </button>
-         		</div>
-         	</div>
-         </div>
-
-   </div>
+							<h5 class"thank-you-font"> Listening is having the person speaking experience being heard <br>- "Bix Bickson"</h2>
+							<h5 class="thank-you-font"> come back and join the conversation </h5>
+              <a class="thank-you-button"> Done </a>
+  </div>
+</div>
