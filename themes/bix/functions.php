5840e2d3488df39578781a15feaa443511234476
wongerchris <wongerchris@gmail.com>
2016-10-01 09:49:25 -0700

    remove updates to none admin users

--- themes/bix/functions.php
+++ themes/bix/functions.php
@@ -130,7 +130,17 @@ require get_template_directory() . '/inc/template-tags.php';
  */
 require get_template_directory() . '/inc/extras.php';
 
+/**
+ * Hide updates from all users but admin 
+ */
 
+function hide_update_notice_to_all_but_admin_users()
+{
+    if (!current_user_can('update_core')) {
+        remove_action( 'admin_notices', 'update_nag', 3 );
+    }
+}
+add_action( 'admin_head', 'hide_update_notice_to_all_but_admin_users', 1 );
 
 
 
