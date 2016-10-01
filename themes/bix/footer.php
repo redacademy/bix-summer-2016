bc3a64d0d7b57789ab0a642306afce9d632a68e0
wongerchris <wongerchris@gmail.com>
2016-10-01 09:24:55 -0700

    add social media links

--- themes/bix/footer.php
+++ themes/bix/footer.php
@@ -25,18 +25,24 @@
 						       </div>
 								 </footer>
 								 <div class ="icons icon-footer">
+								 <a href="https://www.facebook.com/BicksonSquared/?ref=aymt_homepage_panel" target="_blank">
 								 		<span class="fa-stack fa-1x">
 									 		<i class="fa fa-circle fa-stack-2x icon-background"></i>
 											<i class="fa fa-facebook fa-stack-1x"></i>
 										</span>
+									</a>
+									<a href="https://www.linkedin.com/in/bix-bickson-810b5921?authType=NAME_SEARCH&authToken=HVTT&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A77507148%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1474999351820%2Ctas%3Abix" target="_blank">
 										<span class="fa-stack fa-1x">
 									 		<i class="fa fa-circle fa-stack-2x icon-background"></i>
 									 		<i class="fa fa-linkedin fa-stack-1x"></i>
 							 		  </span>
+							 		</a>
+							 		<a href="https://twitter.com/theBixBook" target="_blank">
 							 		<span class="fa-stack fa-1x">
 											<i class="fa fa-circle fa-stack-2x icon-background"></i>
 											<i class="fa fa-twitter fa-stack-1x"></i>
 									</span>
+									</a>
 							 </div>
 	 </div>
  </div>
