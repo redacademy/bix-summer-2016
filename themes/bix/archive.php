791f7522baed27764337a51aeceb1d0b66f79613
Mark <mark.nathaniel.cruz.valdez@gmail.com>
2016-10-03 22:42:20 -0700

    took out code that calls for sidebar

--- themes/bix/archive.php
+++ themes/bix/archive.php
@@ -1,29 +1,34 @@
 <?php
 /**
- * The template for displaying search results pages.
+ * The template for displaying archive pages.
  *
  * @package Bix_Theme
  */
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+				<?php
+					get_template_part( 'template-parts/content' );
+				?>
 
 			<?php endwhile; ?>
 
-			<?php red_starter_numbered_pagination(); ?>
+			<?php the_posts_navigation(); ?>
 
 		<?php else : ?>
 
@@ -32,6 +37,6 @@ get_header(); ?>
 		<?php endif; ?>
 
 		</main><!-- #main -->
-	</section><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
