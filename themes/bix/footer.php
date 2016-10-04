07c21404a29f90b1e774215fe1b2a38f26a09da5
Mark <mark.nathaniel.cruz.valdez@gmail.com>
2016-10-04 00:34:45 -0700

    add lost footer.php

---
+++ themes/bix/footer.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package Bix_Theme
+ */
+?>
+</div><!-- #page -->
+</div><!-- #content -->
+	<div class="footer-content-area">
+		<div class="footer-wrapper">
+			<footer class="footer-form">
+			    <div class="footer-top">
+			        <h1>Let's Talk</h1>
+					<h2> BIX BICKSON </h2>
+					<h2> Bix<span class="footer-orange">{At}</span>Bickson2.Com </h2>
+				</div>
+				<div class ="form-position" id="updates">
+			 		<form action="#" method="post">
+			        	<input type="text" id="email-input" name="email" placeholder="Your Email"/>
+	 					<textarea name="message" placeholder="Drop A Line..."></textarea>
+						<br>
+	 					<button class="footer_button">Send</button>
+			        </form>
+			    </div>
+			</footer>
+			<div class ="icons icon-footer">
+			 	<a href="https://www.facebook.com/BicksonSquared/?ref=aymt_homepage_panel" target="_blank">
+			 		<span class="fa-stack fa-1x">
+				 		<i class="fa fa-circle fa-stack-2x icon-background"></i>
+						<i class="fa fa-facebook fa-stack-1x"></i>
+					</span>
+				</a>
+				<a href="https://www.linkedin.com/in/bix-bickson-810b5921?authType=NAME_SEARCH&authToken=HVTT&locale=en_US&trk=tyah&trkInfo=clickedVertical%3Amynetwork%2CclickedEntityId%3A77507148%2CauthType%3ANAME_SEARCH%2Cidx%3A1-1-1%2CtarId%3A1474999351820%2Ctas%3Abix" target="_blank">
+					<span class="fa-stack fa-1x">
+				 		<i class="fa fa-circle fa-stack-2x icon-background"></i>
+				 		<i class="fa fa-linkedin fa-stack-1x"></i>
+		 		  </span>
+		 		</a>
+		 		<a href="https://twitter.com/theBixBook" target="_blank">
+			 		<span class="fa-stack fa-1x">
+							<i class="fa fa-circle fa-stack-2x icon-background"></i>
+							<i class="fa fa-twitter fa-stack-1x"></i>
+					</span>
+				</a>
+			</div>
+	 </div>
+ </div>
+		<?php wp_footer(); ?>
+	</body>
+</html>
